dbdac698afb0efc7bcf4463bee717fc6692937a5
SAO65 <s.a.olgiati@gmail.com>
2017-12-26 13:17:23 +0100

    26-12-2017

--- MatSocDepEU.R
+++ MatSocDepEU.R
@@ -4,11 +4,11 @@ library(data.table)
 library(plyr)
 
 # Raw Data
-data.MSD <- read.csv(file="ilc_mdsd03.csv", header=TRUE, sep=",")
+data.MSD <- read.csv(file="ilc_mdsd03.csv", header=TRUE, sep=",", stringsAsFactors=FALSE)
 data.MSD
 str(data.MSD)
 
-data.LE <- read.csv(file="demo_mlexpecedu_1_Data.csv", header=TRUE, sep=",")
+data.LE <- read.csv(file="demo_mlexpecedu_1_Data.csv", header=TRUE, sep=",", stringsAsFactors=FALSE)
 data.LE
 str(data.LE)
 
@@ -27,33 +27,54 @@ data.MSD.primary[ , c("AGE","UNIT", "TIME", "SEX", "Flag.and.Footnotes", "ISCED1
 data.MSD.primary <- data.MSD.primary[order(data.MSD.primary[,"GEO"]), ]
 df.MSD.primary <- as.data.frame(data.MSD.primary)
 df.MSD.primary
+df.MSD.primary <- df.MSD.primary[-grep(":",df.MSD.primary$Value),]
+df.MSD.primary
 
 data.MSD.secondary <- DT.MSD[DT.MSD$TIME == "2016" & DT.MSD$ISCED11 == "Upper secondary and post-secondary non-tertiary education (levels 3 and 4)", ] 
 data.MSD.secondary[ , c("AGE","UNIT", "TIME", "SEX", "Flag.and.Footnotes", "ISCED11") := NULL]
 df.MSD.secondary <- as.data.frame(data.MSD.secondary)
+df.MSD.secondary <- df.MSD.secondary[-grep(":",df.MSD.secondary$Value),]
+df.MSD.secondary
 
 data.MSD.tertiary <- DT.MSD[DT.MSD$TIME == "2016" & DT.MSD$ISCED11 == "Tertiary education (levels 5-8)", ] 
 data.MSD.tertiary[ , c("AGE","UNIT", "TIME", "SEX", "Flag.and.Footnotes", "ISCED11") := NULL]
 df.MSD.tertiary <- as.data.frame(data.MSD.tertiary)
+df.MSD.tertiary <- df.MSD.tertiary[-grep(":",df.MSD.tertiary$Value),]
+df.MSD.tertiary
 
 data.LE.primary <- DT.LE[DT.LE$TIME == "2015" & DT.LE$ISCED11 == "Less than primary, primary and lower secondary education (levels 0-2)", ] 
 data.LE.primary[ , c("AGE","UNIT", "TIME", "SEX", "Flag.and.Footnotes", "ISCED11") := NULL]
+data.LE.primary
+data.LE.primary <- data.LE.primary[-grep(":",data.LE.primary$Value),]
 data.LE.primary
 
 data.LE.secondary <- DT.LE[DT.LE$TIME == "2015" & DT.LE$ISCED11 == "Upper secondary and post-secondary non-tertiary education (levels 3 and 4)", ] 
 data.LE.secondary[ , c("AGE","UNIT", "TIME", "SEX", "Flag.and.Footnotes", "ISCED11") := NULL]
 data.LE.secondary
+data.LE.secondary <- data.LE.secondary[-grep(":",data.LE.secondary$Value),]
+data.LE.secondary
 
 data.LE.tertiary <- DT.LE[DT.LE$TIME == "2015" & DT.LE$ISCED11 == "Tertiary education (levels 5-8)", ] 
 data.LE.tertiary[ , c("AGE","UNIT", "TIME", "SEX", "Flag.and.Footnotes", "ISCED11") := NULL]
 data.LE.tertiary
+data.LE.tertiary <- data.LE.tertiary[-grep(":",data.LE.tertiary$Value),]
+data.LE.tertiary
 
-join_all(list(df.MSD.primary ,df.MSD.secondary, df.MSD.tertiary), by='GEO', type='left')
+df <- join_all(list(df.MSD.primary ,
+              df.MSD.secondary, 
+              df.MSD.tertiary,
+              data.LE.primary,
+              data.LE.secondary,
+              data.LE.tertiary), 
+         by='GEO', type='left')
+class(df)
+df
+df <- df[complete.cases(df), ]
+colnames(df) <- c("Country", "MSD_1", "MSD_2", "MSD_3", "LE_1", "LE_2", "LE_3")
+df
 
-
-
-
-
+df$MSD_1 <- as.numeric(as.character(df$MSD_1))
+hist(df$MSD_1)
 
 
 
